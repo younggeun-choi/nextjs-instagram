b798e8287c286424393f84c83cd5f6ae290d00da
YG Choi <yg.choi@dokahn.com>
2023-07-27 19:42:12 +0900

    Debounce

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -5,22 +5,22 @@ import { FormEvent, useState } from "react";
 import useSWR from "swr";
 import GridSpinner from "./GridSpinner";
 import UserCard from "./UserCard";
+import useDebounce from "@/hooks/debounce";
 
 export default function UserSearch() {
-  // /api/search/${keyword} 호출하여 사용자 검색
-  // 검색하는 keyword가 있다면 해당 사용자 정보를 반환
-  // 검색하는 keyword가 없다면 /api/search/ 까지만 호출하여 사용자 목록을 반환
   const [keyword, setKeyword] = useState("");
+  const debouncedKeyword = useDebounce(keyword, 1000);
   const {
     data: users,
     isLoading,
     error,
-  } = useSWR<UserSearchResult[]>(`/api/search/${keyword}`);
+  } = useSWR<UserSearchResult[]>(`/api/search/${debouncedKeyword}`);
 
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
     // useSWR 이 자동으로 keyword 를 갱신하여 호출하므로 아래 코드는 필요 없음
   };
+
   return (
     <section className="w-full max-w-2xl my-4 flex flex-col items-center">
       <form className="w-full mb-4" onSubmit={onSubmit}>
